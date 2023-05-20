1cb3b9f053882065328f14f6f3d7b8d935558195
Carlos Mello <107722227+Carlosedrm@users.noreply.github.com>
2023-05-20 14:29:30 -0300

    Update no estacionamento.sql
    Mais algumas opções para selecionar.

--- estacionamento.sql
+++ estacionamento.sql
@@ -1,9 +1,18 @@
 DROP DATABASE IF EXISTS estacionamento;
 CREATE DATABASE estacionamento;
+
 CREATE USER estacionamento IDENTIFIED BY "estacione";
 DROP USER IF EXISTS estacionamento;
 GRANT ALL ON *.* TO estacionamento WITH GRANT OPTION;
-
-SELECT * FROM restaurant.users;
+SELECT * FROM estacionamento;
 
 SELECT * FROM estacionamento.users;
+SELECT * FROM estacionamento.roles;
+SELECT * FROM estacionamento.user_roles;
+
+
+
+SELECT * FROM estacionamento.consertos;
+SELECT * FROM estacionamento.vagas;
+SELECT * FROM estacionamento.solucoes;
+SELECT * FROM estacionamento.reclamacoes;
