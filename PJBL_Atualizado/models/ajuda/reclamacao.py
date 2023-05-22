fd197122310f17406f051c0fc06431ef2ee16067
Carlos Mello <107722227+Carlosedrm@users.noreply.github.com>
2023-05-22 17:18:26 -0300

    Updated reclamacao.py
    Adicionado o id do úsuario

--- PJBL_Atualizado/models/ajuda/reclamacao.py
+++ PJBL_Atualizado/models/ajuda/reclamacao.py
@@ -1,8 +1,9 @@
-from models import db
+from models import db, User
 
 class Reclamacao(db.Model):
     __tablename__ = 'reclamacoes'
     id = db.Column('id', db.Integer(), primary_key=True)
+    id_user = db.Column(db.Integer(), db.ForeignKey(User.id, ondelete='CASCADE'))
     titulo = db.Column(db.String(20))
     descricao = db.Column(db.String(512))
 
