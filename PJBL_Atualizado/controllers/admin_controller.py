2640cd7a38c7496b413133ac60122263b0b7f8f5
Carlos Mello <107722227+Carlosedrm@users.noreply.github.com>
2023-06-05 10:44:52 -0300

    Updated admin_controller.py
    Agora temos a página de admin

--- PJBL_Atualizado/controllers/admin_controller.py
+++ PJBL_Atualizado/controllers/admin_controller.py
@@ -1,7 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect, url_for, request, flash, session
+from flask_login import login_user, login_required, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from models.auth.user import User
+from models.db import db
 
 admin = Blueprint("admin", __name__, template_folder='./views/', static_folder='./static/', root_path="./")
 
 @admin.route("/")
 def admin_index():
-    return "Admin Blueprint"
+    return render_template("/admin/admin_index.html")
