9e108be3cfe03a993de2a05dca22ad0cd08f9bf6
Carlos Mello <107722227+Carlosedrm@users.noreply.github.com>
2023-06-26 21:08:14 -0300

    Delete PJBL_Atualizado/models directory

--- PJBL_Atualizado/models/estacionamento/vagas.py
+++
@@ -1,7 +0,0 @@
-from models import db, Estacionamento
-
-class Vaga(db.Model):
-    __tablename__ = 'vagas'
-    id = db.Column('id', db.Integer(), primary_key=True)
-    andar = db.Column(db.Integer(), nullable=False)
-    id_estacionamento = db.Column(db.Integer(), db.ForeignKey(Estacionamento.id, ondelete='CASCADE'))
--- PJBL_Atualizado/models/horario/armazena_horario.py
+++
@@ -1,8 +0,0 @@
-from models import db, Horario, Device
-from datetime import datetime
-
-class Armazena(db.Model):
-    __tablename__ = 'armazena_horarios'
-    id = db.Column('id', db.Integer(), primary_key=True)
-    id_horario = db.Column("id_horario", db.Integer(), db.ForeignKey(Horario.id), primary_key=True)
-    id_device = db.Column("id_device", db.Integer(), db.ForeignKey(Device.id), primary_key=True)
